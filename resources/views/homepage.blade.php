e7c7e35beb18ec3dd43ef16d93805d59f0dfbeee
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-01-14 00:00:19 +0100

    Crea sezione shops

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -23,4 +23,28 @@
             LOAD MORE
         </button>
     </section>
+    <section id="shops">
+        <div class="container d-flex justify-content-around">
+            <div class="shop">
+                <img src="{{Vite::asset("resources/img/buy-comics-digital-comics.png")}}" alt="Logo Shop" />
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="shop">
+                <img src="{{Vite::asset("resources/img/buy-comics-merchandise.png")}}" alt="Logo Shop" />
+                <span>DC MERCHANDISE</span>
+            </div>
+            <div class="shop">
+                <img src="{{Vite::asset("resources/img/buy-comics-subscriptions.png")}}" alt="Logo Shop" />
+                <span>SUBSCRIPTION</span>
+            </div>
+            <div class="shop">
+                <img src="{{Vite::asset("resources/img/buy-comics-shop-locator.png")}}" alt="Logo Shop" />
+                <span>COMIC SHOP LOCATOR</span>
+            </div>
+            <div class="shop">
+                <img src="{{Vite::asset("resources/img/buy-dc-power-visa.svg")}}" alt="Logo Shop" />
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+    </section>
 @endsection
