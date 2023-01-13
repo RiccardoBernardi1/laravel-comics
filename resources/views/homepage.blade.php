a57e873195050e51c7eb09d8f696f78e48d2bac0
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-01-13 17:44:41 +0100

    Crea la struttura dell'header

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.main")
+
+@section('page-title')
+    Homepage
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header>
+    <div class="header-top">
+        <div class="container d-flex justify-content-end py-1 align-items-center">
+            <span>DC POWER™ VISA®</span>
+            <div class="dropdown ms-3">
+                <button class="btn dropdown-toggle fw-bold ms-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    ADDITIONAL DC SITES
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container header-bottom d-flex align-items-center">
+        <img class="py-4" src="{{Vite::asset("resources/img/dc-logo.png")}}" alt="Logo DC">
+        <ul class="d-flex ms-ul list-unstyled align-items-center mb-0 fw-bold" >
+            <li><a href="#">CHARACTERS</a> </li>
+            <li><a href="#">COMICS</a> </li>
+            <li><a href="#">MOVIES</a> </li>
+            <li><a href="#">TV</a> </li>
+            <li><a href="#">GAMES</a> </li>
+            <li><a href="#">COLLECTIBLES</a> </li>
+            <li><a href="#">VIDEOS</a> </li>
+            <li><a href="#">FANS</a> </li>
+            <li><a href="#">NEWS</a> </li>
+            <li>
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle fw-bold ms-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        SEARCH
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </li>
+            <input type="search" name="search" id="search" placeholder="Search" class="text-end">
+        </ul>
+    </div>
+</header>
